1174bdacf5c272523b6ce376a55f7fe53431d8af
João Ferreira <joaovfsilva35@gmail.com>
2025-07-11 16:08:00 -0300

    Adicionado transações

--- Gui banco/MainWindow.xaml.cs
+++ Gui banco/MainWindow.xaml.cs
@@ -1,14 +1,8 @@
-﻿using System.Net.Http;
-using System.Text;
+﻿using ConsoleApp1;
+using GalaSoft.MvvmLight.CommandWpf;
+using Gui_banco.View.UserControls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Gui_banco;
 
@@ -17,8 +11,59 @@ namespace Gui_banco;
 /// </summary>
 public partial class MainWindow : Window
 {
+   
+    private ContaDoBanco? Conta { get; }
+    private ContaDoBanco? Conta2 { get; }
+    public ICommand MeuComando { get; }
     public MainWindow()
     {
-        InitializeComponent();
+
+    InitializeComponent();
+       
+        Cliente cliente = new Cliente()
+        {
+            Nome = "João Silva",
+            IdCliente = "12345",
+            Endereço = "Rua Exemplo, 123"
+        };
+        Cliente cliente2 = new Cliente()
+        {
+            Nome = "Brenda",
+            IdCliente = "123456",
+            Endereço = "Rua Exemplo, 123"
+        };
+        ContaDoBanco conta2 = new ContaDoBanco(1001, 500.0, cliente, TipoConta.Corrente);
+        ContaDoBanco conta3 = new ContaDoBanco(1001, 500.0, cliente, TipoConta.Corrente);
+        MeuComando = new RelayCommand(ExecutarBotao);
+        btnConfirmar.BotaoCommand = MeuComando;
+        this.Conta = conta2;
+        this.Conta2 = conta3;
+        ClienteColuna1.nomeCliente.Text = cliente.Nome;
+        ClienteColuna1.enderecoCliente.Text = cliente.Endereço;
+        ClienteColuna1.idCliente.Text = cliente.IdCliente;
+        ClienteColuna2.nomeCliente.Text = cliente2.Nome;
+        ClienteColuna2.enderecoCliente.Text = cliente2.Endereço;
+        ClienteColuna2.idCliente.Text = cliente2.IdCliente;
     }
+
+    private void ExecutarBotao()
+    {
+        Conta.Depositar(double.Parse(btnConfirmar.TextoBox), "Depósito");
+        tSaldo.Saldo = Conta.Saldo;
+    }
+    private void BotaoOperacao_Loaded(object sender, RoutedEventArgs e)
+    {
+        btnConfirmar.tbPlaceholder.Text = "Insirá um valor a depositar";
+        btnConfirmar.Margin = new Thickness(5, 0, 0, 0);
+    }
+
+    private void btnConfirmar_Clicado(object sender, RoutedEventArgs e)
+    {
+    }
+    private void telaSaldo_Loaded(object sender, RoutedEventArgs e)
+    {
+        tSaldo.Saldo = Conta.Saldo;
+    }
+
+
 }
---
+++ Gui banco/View/UserControls/BotaoOperacao.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ConsoleApp1;
+
+namespace Gui_banco.View.UserControls
+{
+    /// <summary>
+    /// Interação lógica para BotaoOperacao.xam
+    /// </summary>
+    public partial class BotaoOperacao : UserControl
+    {
+        public BotaoOperacao()
+        {       
+            InitializeComponent();
+            
+        }
+
+        private string textoBox { get; set; }
+
+        public string TextoBox
+        {
+            get { if(!String.IsNullOrEmpty(textoBox)) 
+                    return textoBox;
+                else return "0";                
+                }
+            set { textoBox = value;
+                tbPlaceholder.Text = textoBox;}
+        }
+
+        private double saldo;
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                TextoBox = txtInput.Text;
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public ICommand BotaoCommand
+        {
+            get { return (ICommand)GetValue(BotaoCommandProperty); }
+            set { SetValue(BotaoCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty BotaoCommandProperty =
+            DependencyProperty.Register("BotaoCommand", typeof(ICommand), typeof(BotaoOperacao), new PropertyMetadata(null));
+
+
+        private void txtInput_TextBoxSomenteNumeros(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !double.TryParse(e.Text, out _); 
+        }
+    }
+}
